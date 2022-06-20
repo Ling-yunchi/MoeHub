72da62993b849cc658203116d15ae1e2316f50ef
Ling-yunchi <wr020210@gmail.com>
2022-06-20 11:49:09 +0800

    init

---
+++ server/src/main/java/tv/moehub/MoeHubApplication.java
@@ -0,0 +1,13 @@
+package tv.moehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoeHubApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/tv/moehub/MoeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.moehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoeHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
